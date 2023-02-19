78cc3c706f6ab279e7d1553c6c1460a20e96547f
kinami <kinami0331@outlook.com>
2023-02-19 17:46:40 +0800

    更改 README 和配置文件

--- next.config.js
+++ next.config.js
@@ -1,14 +1,22 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    }); // 针对 SVG 的处理规则
+    // 修复静态页面的问题
+    trailingSlash: true,
+    exportPathMap: function () {
+        return {
+            "/": {page: "/"},
+            "/lucky": {page: "/lucky"}
+        };
+    },
+    reactStrictMode: true,
+    webpack: (config) => {
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ["@svgr/webpack"]
+        }); // 针对 SVG 的处理规则
 
-    return config;
-  },
+        return config;
+    }
 };
 
 module.exports = nextConfig;
